01199308ae47a8ecc2732941e7cbeb4368b0d06f
AllanCapistrano <allan.capistrano3@gmail.com>
2021-07-24 20:29:18 -0300

    Create Anime entity.

---
+++ src/entities/Anime.ts
@@ -0,0 +1,51 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToMany,
+  JoinTable,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+
+import { Categoty } from "./Category";
+
+@Entity()
+class Anime {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  url: string;
+
+  @Column()
+  description: string;
+
+  @ManyToMany(() => Categoty)
+  @JoinTable()
+  categoty: Categoty[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  /**
+   * Caso seja um novo anime, é gerado um ID para o mesmo.
+   */
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Anime };
