869260adf5f64c5a000d2e4c8aed7604da6f8bb3
AllanCapistrano <allan.capistrano3@gmail.com>
2021-07-25 11:14:52 -0300

    Add create user service.

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,54 @@
+import { getCustomRepository } from "typeorm";
+import { hash } from "bcryptjs";
+import { validate } from "email-validator";
+
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IUser {
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string;
+}
+
+class CreateUserService {
+  /**
+   * Cria um novo usuário no Banco de Dados.
+   * @param user User
+   * @returns User
+   */
+  async execute({ name, email, password, avatar = null }: IUser) {
+    const userRepository = getCustomRepository(UsersRepositories);
+
+    /**
+     * Validando o email.
+     */
+    if(!validate(email)){
+      throw new Error("Email inválido! Tente novamente.");
+    }
+
+    const userAlreadyExists = await userRepository.findOne({ email });
+
+    /**
+     * Verifica se o usuário já está cadastrado (email é único).
+     */
+    if (userAlreadyExists) {
+      throw new Error("Usuário já cadastrado!");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    const user = userRepository.create({
+      name,
+      email,
+      password: passwordHash,
+      avatar,
+    });
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
