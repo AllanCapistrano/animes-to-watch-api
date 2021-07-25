29aa562b1b04bed367fb995b4297e1ad88c6acee
AllanCapistrano <allan.capistrano3@gmail.com>
2021-07-25 11:16:06 -0300

    Create register users route.

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,21 @@
 import { Request, Response, Router } from "express";
 
+import { CreateUserController } from "./controllers/CreateUserController";
+
 const router = Router();
+
+/**
+ * Controllers
+ */
+const createUserController = new CreateUserController();
 
 router.get("/", (request: Request, response: Response) => {
   return response.json({ data: "Hello World" }).status(200);
 });
+
+/**
+ * Rota para registro de novos usuários.
+ */
+router.post("/users/register", createUserController.handle);
 
 export { router };
