59d26c7c873cd127bd85d4bd7208d8a7bcc7baeb
AllanCapistrano <allan.capistrano3@gmail.com>
2021-07-25 12:13:29 -0300

    Add authenticate user controller.

---
+++ src/controllers/AuthenticateUserController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+
+import { AuthenticateUserService } from "../services/AuthenticateUserService";
+
+class AuthenticateUserController {
+  /**
+   * Realiza a comunicação entre as requisições e o serviço de autenticação do
+   * usuário.
+   * @param request Request
+   * @param response Response
+   * @returns JSON
+   */
+  async handle(request: Request, response: Response) {
+    const { email, password } = request.body;
+
+    const authenticateUserService = new AuthenticateUserService();
+
+    const token = await authenticateUserService.execute({
+      email,
+      password,
+    });
+
+    return response.json(token);
+  }
+}
+
+export { AuthenticateUserController };
