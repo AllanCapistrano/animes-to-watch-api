b2fb3c0b3cf566fc2f70db061ca5be4d411da27d
AllanCapistrano <allan.capistrano3@gmail.com>
2021-07-25 11:16:41 -0300

    Add middleware and fix express async errors.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,37 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 
 import "./database";
-import { router } from './routes';
+import { router } from "./routes";
 
 const app = express();
 const PORT = 3000;
 
+/**
+ * Permitindo a utilização de JSON.
+ */
 app.use(express.json());
+/**
+ * Inserindo as rotas criadas no arquivo routes.ts
+ */
 app.use(router);
+
+/**
+ * Middleware para exibição das mensagens geradas pelas exceções.
+ */
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  }
+);
 
 app.listen(PORT, () => console.log(`Server is running on the port ${PORT}...`));
