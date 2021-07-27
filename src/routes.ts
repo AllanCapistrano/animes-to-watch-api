f25e5d9e0939d6bf6b9a5d5f15aa691715adcf67
AllanCapistrano <allan.capistrano3@gmail.com>
2021-07-27 20:13:53 -0300

    Add create category route and comments.

--- src/routes.ts
+++ src/routes.ts
@@ -2,18 +2,30 @@ import { Request, Response, Router } from "express";
 
 import { CreateUserController } from "./controllers/CreateUserController";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateCategoryController } from "./controllers/CreateCategoryController";
 
 const router = Router();
 
+/* --------------------------- Controllers ---------------------------------- */
 /**
- * Controllers
+ * User Controllers.
  */
 const createUserController = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
 
+/**
+ * Category Controllers.
+ */
+const createCategoryController = new CreateCategoryController();
+/* -------------------------------------------------------------------------- */
+
+/* ----------------------------- Routes ------------------------------------- */
+
 router.get("/", (request: Request, response: Response) => {
   return response.json({ data: "Hello World" }).status(200);
 });
+
+/* ------------------------------- User ------------------------------------- */
 
 /**
  * Rota para registro de novos usuários.
@@ -24,5 +36,14 @@ router.post("/users/register", createUserController.handle);
  * Rota para autenticação de usuário.
  */
 router.post("/login", authenticateUserController.handle);
+/* -------------------------------------------------------------------------- */
+
+/* ----------------------------- Category ----------------------------------- */
+
+/**
+ * Rota para criação de uma nova categoria.
+ */
+router.post("/categories/create", createCategoryController.handle);
+/* -------------------------------------------------------------------------- */
 
 export { router };
