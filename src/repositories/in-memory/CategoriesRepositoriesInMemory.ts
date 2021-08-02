07b1bf55c97c91562e4a46132b3ea924a75416b4
AllanCapistrano <allan.capistrano3@gmail.com>
2021-08-02 20:54:02 -0300

    Add categories repositories in memory.

---
+++ src/repositories/in-memory/CategoriesRepositoriesInMemory.ts
@@ -0,0 +1,63 @@
+import { ICategoriesRepositories } from "../interfaces/ICategoriesRepositories";
+import { Category } from "../../entities/Category";
+
+class CategoriesrepositoriesInMemory implements ICategoriesRepositories {
+  private categories: Category[] = [];
+
+  /**
+   * Procura uma categoria pelo nome.
+   * @param name string
+   * @returns Promise<Category>
+   */
+  async findByName(name: string): Promise<Category> {
+    let category: Category | null = null;
+
+    for (let i = 0; i < this.categories.length; i++) {
+      if (this.categories[i].name === name) {
+        category = this.categories[i];
+
+        break;
+      }
+    }
+
+    return category;
+  }
+
+  /**
+   * Cria uma categoria e salva no Array.
+   * @param name string
+   * @returns Promise<Category>
+   */
+  async createAndSave(name: string): Promise<Category> {
+    let category: Category = new Category();
+
+    category.name = name;
+
+    this.categories.push(category);
+
+    return category;
+  }
+
+  /**
+   * Retorna todas as categorias cujos IDs foram passados.
+   * @param categories string[]
+   * @returns Promise<Category[]>
+   */
+  async findMultiples(categories: string[]): Promise<Category[]> {
+    let categoriesInMemory: Category[] = null;
+
+    for (let i = 0; i < this.categories.length; i++) {
+      for (let j = 0; j < categories.length; j++) {
+        if (this.categories[i].id === categories[j]) {
+          categoriesInMemory.push(this.categories[i]);
+
+          break;
+        }
+      }
+    }
+
+    return categoriesInMemory;
+  }
+}
+
+export { CategoriesrepositoriesInMemory };
