34af86da5eb27a0c10aa909a22386694825686c4
AllanCapistrano <allan.capistrano3@gmail.com>
2021-08-03 11:42:02 -0300

    Add forgot password service.

---
+++ src/services/ForgotPasswordService.ts
@@ -0,0 +1,45 @@
+import { validate } from "email-validator";
+import { hash } from "bcryptjs";
+
+import { IUsersRepositories } from "../repositories/interfaces/IUsersRepositories";
+
+interface IUser {
+  email: string;
+  newPassword: string;
+}
+
+class ForgotPasswordService {
+  /**
+   * Método construtor.
+   * @param userRepository IUsersRepositories
+   */
+  constructor(private userRepository: IUsersRepositories) {}
+
+  /**
+   * Altera a senha do usuário no Bando de Dados.
+   * @param user User
+   * @returns Promise<boolean>
+   */
+  async execute({ email, newPassword }: IUser) {
+    if (!validate(email)) {
+      throw new Error("Email inválido! Tente novamente.");
+    }
+
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Usuário não encontrado! Verifique o email digitado.");
+    }
+
+    const passwordHash = await hash(newPassword, 8);
+
+    const isUserUpdated = await this.userRepository.changePassword(
+      user,
+      passwordHash
+    );
+
+    return isUserUpdated;
+  }
+}
+
+export { ForgotPasswordService };
