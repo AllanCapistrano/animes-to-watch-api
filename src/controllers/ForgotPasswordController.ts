4ffaf174333490757b69d3a2c0f284f3fbf7b432
AllanCapistrano <allan.capistrano3@gmail.com>
2021-08-03 11:44:07 -0300

    Add forgort password controller.

---
+++ src/controllers/ForgotPasswordController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+
+import { ForgotPasswordService } from "../services/ForgotPasswordService";
+
+class ForgotPasswordController {
+  /**
+   * Método construtor.
+   * @param forgotPasswordService ForgotPasswordService
+   */
+  constructor(private forgotPasswordService: ForgotPasswordService) {}
+
+  /**
+   * Realiza a comunicação entre as requisições e o serviço de alteração de
+   * senhas.
+   * @param request Request
+   * @param response Response
+   * @returns JSON
+   */
+  async handle(request: Request, response: Response) {
+    const { email, password } = request.body;
+
+    const isUserUpdated = await this.forgotPasswordService.execute({
+      email,
+      newPassword: password,
+    });
+
+    if (!isUserUpdated) {
+      return response.status(500).send({
+        error: "O servidor não conseguiu alterar a senha.",
+      });
+    }
+
+    return response.status(200).send({
+      data: "Senha alterada com sucesso.",
+    });
+  }
+}
+
+export { ForgotPasswordController };
