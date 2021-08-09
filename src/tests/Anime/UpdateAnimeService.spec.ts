a604432179f681ca38741684c75674df5ffa95e2
AllanCapistrano <allan.capistrano3@gmail.com>
2021-08-08 22:19:12 -0300

    Add tests o update anime service.

---
+++ src/tests/Anime/UpdateAnimeService.spec.ts
@@ -0,0 +1,136 @@
+import { IAnimesRepositories } from "../../repositories/interfaces/IAnimesRepositories";
+import { ICategoriesRepositories } from "../../repositories/interfaces/ICategoriesRepositories";
+import { IAnimesCategoriesRepositories } from "../../repositories/interfaces/IAnimesCategoriesRepositories";
+import { UpdateAnimeService } from "../../services/UpdateAnimeService";
+import { AnimesRepositoriesInMemory } from "../../repositories/in-memory/AnimesRepositoriesInMemory";
+import { CategoriesRepositoriesInMemory } from "../../repositories/in-memory/CategoriesRepositoriesInMemory";
+import { AnimesCategoriesRepositoriesInMemory } from "../../repositories/in-memory/AnimesCategoriesRepositoriesInMemory";
+import { CreateAnimeService } from "../../services/CreateAnimeService";
+import { CreateCategoryService } from "../../services/CreateCategoryService";
+import { Anime } from "../../entities/Anime";
+
+describe("Update anime", () => {
+  let animeRepository: IAnimesRepositories;
+  let categoryRepository: ICategoriesRepositories;
+  let animeCategoryRepository: IAnimesCategoriesRepositories;
+  let updateAnimeService: UpdateAnimeService;
+  let createAnimeService: CreateAnimeService;
+  let createCategoryService: CreateCategoryService;
+  let categoriesIds: string[] = [];
+  let c0, c1, c2;
+  let anime: Anime[] = [];
+
+  beforeAll(async () => {
+    animeRepository = new AnimesRepositoriesInMemory();
+    categoryRepository = new CategoriesRepositoriesInMemory();
+    animeCategoryRepository = new AnimesCategoriesRepositoriesInMemory();
+    updateAnimeService = new UpdateAnimeService(
+      animeRepository,
+      categoryRepository,
+      animeCategoryRepository
+    );
+    createAnimeService = new CreateAnimeService(
+      animeRepository,
+      categoryRepository,
+      animeCategoryRepository
+    );
+    createCategoryService = new CreateCategoryService(categoryRepository);
+
+    c0 = (await createCategoryService.execute({ name: "category0" })).id;
+    c1 = (await createCategoryService.execute({ name: "category1" })).id;
+    c2 = (await createCategoryService.execute({ name: "category2" })).id;
+
+    categoriesIds.push(c0);
+    categoriesIds.push(c1);
+    categoriesIds.push(c2);
+
+    const anime0Data = {
+      name: "Test0",
+      image: "https://images.com/animes/0",
+      url: "https://myanime.com/anime/0",
+      description: "Here is a simple description",
+      categories: categoriesIds,
+    };
+
+    const anime1Data = {
+      name: "Test1",
+      image: "https://images.com/animes/1",
+      url: "https://myanime.com/anime/1",
+      categories: [c1, c2],
+    };
+
+    anime[0] = await createAnimeService.execute(anime0Data);
+    anime[1] = await createAnimeService.execute(anime1Data);
+  });
+
+  it("Should be able to update an anime", async () => {
+    anime[0].name = "Test New Name";
+    anime[0].image = null;
+    anime[0].url = "https://newimage.com/anime/0";
+    anime[0].description = null;
+
+    await updateAnimeService.execute({ ...anime[0], categories: [c0] });
+
+    expect(anime[0].name).toBe("Test New Name");
+
+    expect(anime[0].image).toBeNull();
+
+    expect(anime[0].url).toBe("https://newimage.com/anime/0");
+
+    expect(anime[0].description).toBeNull();
+  });
+
+  /**
+   * AINDA FALTA!!!
+   */
+  // it("Should not be able to update an anime name that already exists", async () => {
+  //   // console.log(anime)
+
+  //   anime[0].name = "Test1";
+  //   // anime[0].image = null;
+  //   // anime[0].url = "https://newimage.com/anime/0";
+  //   // anime[0].description = null;
+
+  //   // await expect(
+  //   //   updateAnimeService.execute({ name: anime[0].name, ...anime[0], categories: [c0] })
+  //   // ).rejects.toEqual(
+  //   //   new Error("Já existe um anime cadastrado com esse nome! Tente novamente.")
+  //   // );
+
+  //   await updateAnimeService.execute({ ...anime[0], categories: [c0] });
+
+  //   expect(anime[0].name).toBe("Test1");
+  // });
+
+  it("Should not be able to update an anime with empty categories", async () => {
+    anime[1].name = "New Name";
+    anime[1].image = null;
+    anime[1].url = "https://newimage.com/anime/1";
+    anime[1].description = null;
+
+    await expect(
+      updateAnimeService.execute({ ...anime[1], categories: [] })
+    ).rejects.toEqual(
+      new Error(
+        "Para a atualização de um anime é necessário uma ou mais categorias."
+      )
+    );
+  });
+
+  it("Should not be able to update an anime with invalid categories", async () => {
+    anime[1].name = "New Name";
+    anime[1].image = null;
+    anime[1].url = "https://newimage.com/anime/1";
+    anime[1].description = null;
+
+    await expect(
+      updateAnimeService.execute({
+        ...anime[1],
+        categories: [
+          "9b954b24-fc5e-4927-8b45-4859c5b2b859",
+          "e92dd749-a7c8-42c9-b56b-1dfda7920233",
+        ],
+      })
+    ).rejects.toEqual(new Error("Categoria(s) inválida(s)! Tente novamente."));
+  });
+});
