bb61ad62ee0b7fb78f7e6020dd41eb2543a07f75
AllanCapistrano <allan.capistrano3@gmail.com>
2021-08-17 11:18:18 -0300

    Add remove category service.

---
+++ src/services/RemoveCategoryService.ts
@@ -0,0 +1,48 @@
+import { ICategoriesRepositories } from "../repositories/interfaces/ICategoriesRepositories";
+
+interface ICategory {
+  id: string | null;
+  name?: string;
+}
+
+class RemoveCategoryService {
+  /**
+   * Método Construtor.
+   * @param categoryRepository ICategoriesRepositories
+   */
+  constructor(private categoryRepository: ICategoriesRepositories) {}
+
+  /**
+   * Remove uma categoria.
+   * @param category Category
+   * @returns boolean
+   */
+  async execute({ id, name = null }: ICategory) {
+    if (id) {
+      const categoryExists = await this.categoryRepository.categoryExists(id);
+
+      if (!categoryExists) {
+        throw new Error("Categoria não encontrada! Tente novamente.");
+      }
+
+      return await this.categoryRepository.removeCategory(categoryExists);
+    }
+
+    if (name) {
+      const categoryExists = await this.categoryRepository.categoryExists(
+        null,
+        name
+      );
+
+      if (!categoryExists) {
+        throw new Error("Categoria não encontrada! Tente novamente.");
+      }
+
+      return await this.categoryRepository.removeCategory(categoryExists);
+    }
+
+    return false;
+  }
+}
+
+export { RemoveCategoryService };
